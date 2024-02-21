d71f7488a56e8206180b81fbb269c6d1f113827c
Julian Claus <kontakt@julian-claus.de>
2024-02-21 09:31:47 +0100

    chore: wip

--- app/web/src/plugins/vuetify.js
+++ app/web/src/plugins/vuetify.js
@@ -3,8 +3,10 @@ import 'vuetify/styles'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { createVuetify } from 'vuetify'
+import { md3 } from 'vuetify/blueprints'
 
 export default createVuetify({
+  blueprint: md3,
   components: {
     ...components
   },
