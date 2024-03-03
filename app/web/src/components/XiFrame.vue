26e22a1ceb3f6718e590173dbeacf79000f75e5a
Julian Claus <kontakt@julian-claus.de>
2024-03-03 06:45:55 +0100

    chore: wip

---
+++ app/web/src/components/XiFrame.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  src: String
+})
+</script>
+
+<template>
+</template>
+
+<style>
+</style>
--- app/web/src/views/ContributionsView.vue
+++ app/web/src/views/ContributionsView.vue
@@ -155,6 +155,14 @@
               v-if="technology !== 'go' && technology !== 'node'">
               <p v-html="selectedApiDocumentation" />
             </v-card-text><br v-if="technology === 'go'">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/QWCZbZE--8I?si=7vi7BQxp65CRJav9&amp;controls=0"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen />
           </v-card>
 
           <v-skeleton-loader
