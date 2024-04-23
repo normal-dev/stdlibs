566289aa549c7d3037c6c8c36a58d7623ae91321
Julian Claus <kontakt@julian-claus.de>
2024-04-23 12:05:44 +0200

    feat: add news

--- app/web/src/views/NewsView.vue
+++ app/web/src/views/NewsView.vue
@@ -1,5 +1,41 @@
 <script setup>
 document.title = 'stdlibs.com - News'
+
+const news = [
+  {
+    title: 'Go 1.22 released',
+    date: '2024-02-06',
+    text: `Go 1.22 fixed the "for" loop semantics, allows to range over
+      integers, comes with perfomance improvements and the new
+      math/rand/v2 package as well as a net/http server mocker.
+      net/http.ServerMux accepts now methods and wildcards.`
+  },
+  {
+    title: 'We added three new contributions',
+    date: '2024-01-02',
+    text: `For Golang, we added traefik/traefik and cloudflare/cloudflare
+      with net package contributions. moby/moby, canonical/lxd and
+      containerd/containerd are using a lot of I/O calls. While
+      gopherjs/gopherjs makes a usage of the go/ast package.`
+  },
+  {
+    title: 'We added five new contributions',
+    date: '2023-12-13',
+    text: `spf13/cobra adds reflect contributions for Golang and
+      vercel/next.js with node:http and node:https for Node.js.
+      Microsofts microsoft/code imports node:worker_threads, while
+      prettier/prettier leverages the file system API. The last addition
+      is ueber-go/zap with time, fmt and bytes usage.`
+  },
+  {
+    title: 'Node.js 20.9.0 released',
+    date: '2023-10-24',
+    text: `Node.js version 20.9.0 marked the switch to Long-Term Support (LTS),
+    meaning it will receive critical bug fixes for an extended period. This
+    release also introduced a new crypto.hash() function for faster one-time
+    hashing of smaller inputs.`
+  }
+]
 </script>
 
 <template>
@@ -14,72 +50,18 @@ document.title = 'stdlibs.com - News'
 
       <v-row class="mt-4">
         <v-col
+          v-for="(article, index) in news"
+          :key="index"
           sm="12"
-          md="6">
-          <v-card
-            flat>
+          md="4">
+          <v-card flat>
             <v-card-item>
-              <v-card-title>
-                Go 1.22 released
-              </v-card-title>
-              <v-card-subtitle>
-                Written 02/06/2024
-              </v-card-subtitle>
+              <v-card-title>{{ article.title }}</v-card-title>
+              <v-card-subtitle>Written at {{ article.date }}</v-card-subtitle>
             </v-card-item>
 
             <v-card-text>
-              Go 1.22 fixed the "for" loop semantics, allows to range over
-              integers, comes with perfomance improvements and the new
-              math/rand/v2 package as well as a net/http server mocker.
-              net/http.ServerMux accepts now methods and wildcards.
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col
-          sm="12"
-          md="6">
-          <v-card
-            flat>
-            <v-card-item>
-              <v-card-title>
-                We added three new contributions
-              </v-card-title>
-              <v-card-subtitle>
-                Written 04/01/2024
-              </v-card-subtitle>
-            </v-card-item>
-
-            <v-card-text>
-              For Golang, we added traefik/traefik and cloudflare/cloudflare with
-              net package contributions. moby/moby, canonical/lxd and
-              containerd/containerd are using a lot of I/O calls. While
-              gopherjs/gopherjs makes a usage of the go/ast package.
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col
-          sm="12"
-          md="6">
-          <v-card
-            flat>
-            <v-card-item>
-              <v-card-title>
-                We added five new contributions
-              </v-card-title>
-              <v-card-subtitle>
-                Written 04/01/2024
-              </v-card-subtitle>
-            </v-card-item>
-
-            <v-card-text>
-              spf13/cobra adds reflect contributions for Golang and
-              vercel/next.js with node:http and node:https for Node.js.
-              Microsofts microsoft/code imports node:worker_threads, while
-              prettier/prettier leverages the file system API. The last addition
-              is ueber-go/zap with time, fmt and bytes usage.
+              {{ article.text }}
             </v-card-text>
           </v-card>
         </v-col>
