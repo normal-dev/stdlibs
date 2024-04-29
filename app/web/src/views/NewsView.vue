1add23e72cfbd1d8cda5030c24990cae6fc5495e
Julian Claus <kontakt@julian-claus.de>
2024-04-29 09:34:43 +0200

    fix: news

--- app/web/src/views/NewsView.vue
+++ app/web/src/views/NewsView.vue
@@ -7,42 +7,42 @@ const news = [
     title: 'We added four new contributions',
     date: '2024-04-15',
     text: `The famous logger prometheus/prometheus has been added to the list of
-      Go contributions. Also for Go, rclone/rclone uses the file system API. For
-      Node.js we added angular/angular with path, http and fs API calls, as well
-      as sveltejs/svelte with assertion usages.`
+  Go contributions. Also for Go, rclone/rclone uses the file system API. For
+  Node.js we added angular/angular with path, http and fs API calls, as well
+  as sveltejs/svelte with assertion usages.`
   },
   {
     title: 'Go 1.22 released',
     date: '2024-02-06',
     text: `Go 1.22 fixed the "for" loop semantics, allows to range over
-      integers, comes with perfomance improvements and the new
-      math/rand/v2 package as well as a net/http server mocker.
-      net/http.ServerMux accepts now methods and wildcards.`
+integers, comes with perfomance improvements and the new
+math/rand/v2 package as well as a net/http server mocker.
+net/http.ServerMux accepts now methods and wildcards.`
   },
   {
     title: 'We added three new contributions',
     date: '2024-01-02',
     text: `For Golang, we added traefik/traefik and cloudflare/cloudflare
-      with net package contributions. moby/moby, canonical/lxd and
-      containerd/containerd are using a lot of I/O calls. While
-      gopherjs/gopherjs makes a usage of the go/ast package.`
+with net package contributions. moby/moby, canonical/lxd and
+containerd/containerd are using a lot of I/O calls. While
+gopherjs/gopherjs makes a usage of the go/ast package.`
   },
   {
     title: 'We added five new contributions',
     date: '2023-12-13',
     text: `spf13/cobra adds reflect contributions for Golang and
-      vercel/next.js with node:http and node:https for Node.js.
-      Microsofts microsoft/code imports node:worker_threads, while
-      prettier/prettier leverages the file system API. The latest addition
-      is ueber-go/zap with time, fmt and bytes usages.`
+vercel/next.js with node:http and node:https for Node.js.
+Microsofts microsoft/code imports node:worker_threads, while
+prettier/prettier leverages the file system API. The latest addition
+is ueber-go/zap with time, fmt and bytes usages.`
   },
   {
     title: 'Node.js 20.9.0 released',
     date: '2023-10-24',
     text: `Node.js version 20.9.0 marked the switch to Long-Term Support (LTS),
-    meaning it will receive critical bug fixes for an extended period. This
-    release also introduced a new crypto.hash() function for faster one-time
-    hashing of smaller inputs.`
+meaning it will receive critical bug fixes for an extended period. This
+release also introduced a new crypto.hash() function for faster one-time
+hashing of smaller inputs.`
   }
 ]
 </script>
@@ -54,6 +54,7 @@ const news = [
         <v-col
           v-for="(article, index) in news"
           :key="index"
+          cols="12"
           sm="12"
           md="4">
           <v-card flat>
