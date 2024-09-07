8de746be1055e094cf3fcd08730c9745b4648d7a
Julian Claus <kontakt@julian-claus.de>
2024-09-07 06:34:04 +0200

    chore: wip

--- node/contribs/extract.test.mjs
+++ node/contribs/extract.test.mjs
@@ -79,30 +79,30 @@ describe('ImportDefaultSpecifier', undefined, () => {
       ])
     })
 
-    //     test('VariableDeclaration', () => {
-    //       assert.deepEqual(extract(openTest('default/global/VariableDeclaration.js')), [
-    //         {
-    //           ident: 'node:assert.default',
-    //           line: 3
-    //         }
-    //       ])
-    //     })
-    //   })
+    test('VariableDeclaration', () => {
+      assert.deepEqual(extract(openTest('default/global/VariableDeclaration.js')), [
+        {
+          ident: 'node:assert.default',
+          line: 3
+        }
+      ])
+    })
+  })
 
-    //   describe('local', undefined, () => {
-    //     test('ObjectProperty', () => {
-    //       assert.deepEqual(extract(openTest('default/local/ObjectProperty.js')), [{
-    //         ident: 'node:assert.default',
-    //         line: 5
-    //       }])
-    //     })
+  describe('local', undefined, () => {
+    test('ObjectProperty', () => {
+      assert.deepEqual(extract(openTest('default/local/ObjectProperty.js')), [{
+        ident: 'node:assert.default',
+        line: 5
+      }])
+    })
 
-    //     test('VariableDeclaration', () => {
-    //       assert.deepEqual(extract(openTest('default/local/VariableDeclaration.js')), [{
-    //         ident: 'node:assert.default',
-    //         line: 4
-    //       }])
-    //     })
+    test('VariableDeclaration', () => {
+      assert.deepEqual(extract(openTest('default/local/VariableDeclaration.js')), [{
+        ident: 'node:assert.default',
+        line: 4
+      }])
+    })
   })
 })
 
@@ -157,106 +157,106 @@ describe('ImportSpecifier', undefined, () => {
 })
 
 describe('proprietary', undefined, () => {
-  // test('build.js', () => {
-  //   assert.deepEqual(extract(openTest('proprietary/build.js')), [
-  //     {
-  //       ident: 'node:fs/promises.mkdir',
-  //       line: 50
-  //     },
-  //     {
-  //       ident: 'node:fs/promises.rm',
-  //       line: 111
-  //     },
-  //     {
-  //       ident: 'node:fs/promises.readFile',
-  //       line: 160
-  //     },
-  //     {
-  //       ident: 'node:fs/promises.writeFile',
-  //       line: 175
-  //     },
-  //     {
-  //       ident: 'node:fs.existsSync',
-  //       line: 110
-  //     },
-  //     {
-  //       ident: 'node:fs.existsSync',
-  //       line: 157
-  //     },
-  //     {
-  //       ident: 'node:path.resolve',
-  //       line: 45
-  //     },
-  //     {
-  //       ident: 'node:path.resolve',
-  //       line: 101
-  //     },
-  //     {
-  //       ident: 'node:path.resolve',
-  //       line: 149
-  //     },
-  //     {
-  //       ident: 'node:path.basename',
-  //       line: 161
-  //     },
-  //     {
-  //       ident: 'node:path.resolve',
-  //       line: 176
-  //     },
-  //     {
-  //       ident: 'node:zlib.gzipSync',
-  //       line: 163
-  //     },
-  //     {
-  //       ident: 'node:zlib.brotliCompressSync',
-  //       line: 164
-  //     },
-  //     {
-  //       ident: 'node:os.cpus',
-  //       line: 79
-  //     },
-  //     {
-  //       ident: 'node:module.createRequire',
-  //       line: 32
-  //     }
-  //   ])
-  // })
+  test('build.js', () => {
+    assert.deepEqual(extract(openTest('proprietary/build.js')), [
+      {
+        ident: 'node:fs/promises.mkdir',
+        line: 50
+      },
+      {
+        ident: 'node:fs/promises.rm',
+        line: 111
+      },
+      {
+        ident: 'node:fs/promises.readFile',
+        line: 160
+      },
+      {
+        ident: 'node:fs/promises.writeFile',
+        line: 175
+      },
+      {
+        ident: 'node:fs.existsSync',
+        line: 110
+      },
+      {
+        ident: 'node:fs.existsSync',
+        line: 157
+      },
+      {
+        ident: 'node:path.resolve',
+        line: 45
+      },
+      {
+        ident: 'node:path.resolve',
+        line: 101
+      },
+      {
+        ident: 'node:path.resolve',
+        line: 149
+      },
+      {
+        ident: 'node:path.basename',
+        line: 161
+      },
+      {
+        ident: 'node:path.resolve',
+        line: 176
+      },
+      {
+        ident: 'node:zlib.gzipSync',
+        line: 163
+      },
+      {
+        ident: 'node:zlib.brotliCompressSync',
+        line: 164
+      },
+      {
+        ident: 'node:os.cpus',
+        line: 79
+      },
+      {
+        ident: 'node:module.createRequire',
+        line: 32
+      }
+    ])
+  })
 
-  // test('did-calypso-app-change.mjs', () => {
-  //   assert.deepEqual(extract(openTest('proprietary/did-calypso-app-change.mjs')), [
-  //     {
-  //       ident: 'node:child_process.exec',
-  //       line: 6
-  //     },
-  //     {
-  //       ident: 'node:fs.createWriteStream',
-  //       line: 29
-  //     },
-  //     {
-  //       ident: 'node:stream.Readable',
-  //       line: 40
-  //     },
-  //     {
-  //       ident: 'node:stream/promises.finished',
-  //       line: 40
-  //     },
-  //     {
-  //       ident: 'node:util.promisify',
-  //       line: 6
-  //     }
-  //   ])
-  // })
+  test('did-calypso-app-change.mjs', () => {
+    assert.deepEqual(extract(openTest('proprietary/did-calypso-app-change.mjs')), [
+      {
+        ident: 'node:child_process.exec',
+        line: 6
+      },
+      {
+        ident: 'node:fs.createWriteStream',
+        line: 29
+      },
+      {
+        ident: 'node:stream.Readable',
+        line: 40
+      },
+      {
+        ident: 'node:stream/promises.finished',
+        line: 40
+      },
+      {
+        ident: 'node:util.promisify',
+        line: 6
+      }
+    ])
+  })
 
-  // test('goods.ts', () => {
-  //   assert.deepEqual(extract(openTest('proprietary/goods.ts')), [
-  //     {
-  //       ident: 'node:assert.default',
-  //       line: 143
-  //     },
-  //     {
-  //       ident: 'node:readline.createInterface',
-  //       line: 96
-  //     }
-  //   ])
-  // })
+  test('goods.ts', () => {
+    assert.deepEqual(extract(openTest('proprietary/goods.ts')), [
+      {
+        ident: 'node:assert.default',
+        line: 143
+      },
+      {
+        ident: 'node:readline.createInterface',
+        line: 96
+      }
+    ])
+  })
 })
