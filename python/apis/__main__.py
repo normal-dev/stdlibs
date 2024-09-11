14e6801717ab2c64feda85a108914980472f716e
Julian Claus <kontakt@julian-claus.de>
2024-09-11 17:30:21 +0200

    chore: wip

--- python/apis/__main__.py
+++ python/apis/__main__.py
@@ -2,6 +2,7 @@ import importlib
 import sys
 import inspect
 import os
+import types
 
 sys.path.append(os.path.abspath(".."))
 
@@ -15,22 +16,34 @@ def get_type(value):
   try:
     return type(value).__name__
   except:
-    if inspect.isbuiltin(value) or inspect.ismethod(value):
+    if inspect.isbuiltin(value) or inspect.ismethod(value) or isinstance(value, types.FunctionType):
       return "function"
     elif inspect.isclass(value):
       return "class"
     elif inspect.ismodule(value):
       return "module"
+    elif isinstance(value, str):
+      return "string"
+    elif isinstance(value, int):
+      return "int"
+    elif isinstance(value, float):
+      return "float"
+    elif isinstance(value, list):
+      return "list"
+    elif isinstance(value, dict):
+      return "dict"
+    elif value is None:
+      return "none"
     else:
-      return type(value)
+        print(type(value))
 
 mongo_coll.delete_many({})
 
 for module_name in sys.stdlib_module_names:
-  if module_name.startswith("_") or module_name == "antigravity":
+  if module_name.startswith("_") or module_name is "antigravity":
     continue
 
-  if module_name != "math":
+  if importlib.find_loader(module_name) is None:
     continue
 
   module = importlib.import_module(module_name)
