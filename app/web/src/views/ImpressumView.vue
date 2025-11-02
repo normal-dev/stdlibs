ea2ed430b9e4edb1d4bfc4860bb1ca196385f386
Julian Claus <kontakt@julian-claus.de>
2025-11-02 19:41:02 +0100

    feat(web): impressum

--- app/web/src/views/ImpressumView.vue
+++ app/web/src/views/ImpressumView.vue
@@ -13,8 +13,8 @@ document.title = 'stdlibs.com - Impressum'
         </v-card-title>
         <v-card-text>
           normal dev e.K.<br>
-          Sophie-Charlotten-Straße 37<br>
-          14057 Berlin<br>
+          Dahlemer Weg 212B<br>
+          14167 Berlin<br>
           &#107;&#111;&#110;&#116;&#97;&#107;&#116;&commat;&#106;&#117;&#108;&#105;&#97;&#110;&#45;&#99;&#108;&#97;&#117;&#115;&period;&#100;&#101;
         </v-card-text>
       </v-card>
