39ca43fda9cad2e0d718f8ae69e74f2d21438102
Julian Claus <kontakt@julian-claus.de>
2025-11-08 17:29:15 +0100

    feat: remove address

--- app/web/src/views/ImpressumView.vue
+++ app/web/src/views/ImpressumView.vue
@@ -13,21 +13,7 @@ document.title = 'stdlibs.com - Impressum'
         </v-card-title>
         <v-card-text>
           normal dev e.K.<br>
-          Dahlemer Weg 212B<br>
-          14167 Berlin<br>
           &#107;&#111;&#110;&#116;&#97;&#107;&#116;&commat;&#106;&#117;&#108;&#105;&#97;&#110;&#45;&#99;&#108;&#97;&#117;&#115;&period;&#100;&#101;
-        </v-card-text>
-      </v-card>
-
-      <v-card
-        flat
-        variant="text">
-        <v-card-title>
-          Legal
-        </v-card-title>
-        <v-card-text>
-          German trade register: HRA 55484 B, 14057 Charlottenburg (Berlin)<br>
-          VAT identification:  HRA 55484 B, DE321639936
         </v-card-text>
       </v-card>
     </v-container>
